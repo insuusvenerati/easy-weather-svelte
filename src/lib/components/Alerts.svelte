df233fc9646e5e6a15d5aec3f6f3dc02109ffaac
Sean Norwood <norwood.sean@gmail.com>
2023-09-01 00:33:56 -0500

    Use skeleton scrolling for alerts

--- src/lib/components/Alerts.svelte
+++ src/lib/components/Alerts.svelte
@@ -4,17 +4,19 @@
 	export let alerts: Alert[];
 </script>
 
-{#each alerts as alert}
-	<div class="alert">
-		<p class="alert-title">{alert.title}</p>
-		<p>Regions: {alert.regions.join(', ')}</p>
-		<p>Severity: {alert.severity}</p>
-		<p>Time: {new Date(alert.time * 1000).toLocaleString()}</p>
-		<p>Expires: {new Date(alert.expires * 1000).toLocaleString()}</p>
-		<p>Description: {alert.description}</p>
-		<p><a class="underline" href={alert.uri} target="_blank">More info</a></p>
-	</div>
-{/each}
+<div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10">
+	{#each alerts as alert}
+		<div class="alert">
+			<p class="alert-title">{alert.title}</p>
+			<p>Regions: {alert.regions.join(', ')}</p>
+			<p>Severity: {alert.severity}</p>
+			<p>Time: {new Date(alert.time * 1000).toLocaleString()}</p>
+			<p>Expires: {new Date(alert.expires * 1000).toLocaleString()}</p>
+			<p>Description: {alert.description}</p>
+			<p><a class="underline" href={alert.uri} target="_blank">More info</a></p>
+		</div>
+	{/each}
+</div>
 
 <style>
 	.alert {
