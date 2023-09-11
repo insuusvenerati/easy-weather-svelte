3bb4f0148a2b23adca5a133316229e43bb40d1f6
Sean Norwood <norwood.sean@gmail.com>
2023-09-11 09:53:28 -0500

    Stop using trpc

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,6 @@
-// hooks.server.ts
-import { createContext } from '$lib/trpc/context';
-import { router } from '$lib/trpc/router';
 import type { Handle } from '@sveltejs/kit';
-import { sequence } from '@sveltejs/kit/hooks';
-import { createTRPCHandle } from 'trpc-sveltekit';
 
-const trpcHandle: Handle = createTRPCHandle({ router, createContext });
-
-const themeHandler: Handle = async ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
 	let theme = '';
 
 	const cookieTheme = event.cookies.get('theme');
@@ -23,5 +16,3 @@ const themeHandler: Handle = async ({ event, resolve }) => {
 		transformPageChunk: ({ html }) => html.replace('data-theme=""', `data-theme="${theme}"`)
 	});
 };
-
-export const handle = sequence(trpcHandle, themeHandler);
