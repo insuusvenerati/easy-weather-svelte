60aadea81477057e7ae59e9529bc220135ee7805
Sean Norwood <norwood.sean@gmail.com>
2023-09-11 13:38:05 -0500

    Handle issues when coords not found

--- src/routes/weather/[zipcode]/+page.server.ts
+++ src/routes/weather/[zipcode]/+page.server.ts
@@ -1,11 +1,15 @@
 import { getCoordsByZipcode } from '$lib/util';
 import { getWeather } from '$lib/util.server';
+import invariant from 'tiny-invariant';
 import type { PageServerLoad } from './$types';
 
 export const load = (async (event) => {
 	const { params } = event;
 	const { zipcode } = params;
-	const { lat, lon } = await getCoordsByZipcode(zipcode);
+	const coords = await getCoordsByZipcode(zipcode);
+
+	invariant(coords, 'No coordinates found for zipcode');
+	const { lat, lon } = coords;
 
 	return {
 		streamed: { weather: getWeather({ lat, lon }) }
