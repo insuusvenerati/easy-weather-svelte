b7a21a64f599a20cdf92b908d7210a50fa9538b8
Sean Norwood <norwood.sean@gmail.com>
2024-02-21 10:03:51 -0600

    Update deps and use node server
    Signed-off-by: Sean Norwood <norwood.sean@gmail.com>

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,22 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess()],
 
 	vitePlugin: {
 		inspector: true
 	},
 	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: adapter()
+		// adapter: adapter({
+		// 	// See below for an explanation of these options
+		// 	routes: {
+		// 		include: ['/*'],
+		// 		exclude: ['<all>']
+		// 	}
+		// })
 	}
 };
 export default config;
